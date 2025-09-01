8e91f41f2c50415eb59bed5980ca6789a3a7d976
Thomas Mello <work.mello@gmail.com>
2025-09-02 02:41:19 +0300

    Bumped version from 6.3.1 → 6.3.2.

--- src/markup/yt_clipper.ts
+++ src/markup/yt_clipper.ts
@@ -3,8 +3,8 @@
 // BANNER GUARD
 // @locale       english
 // @name         nv_clipper
-// @version      6.3.1
-// @version      6.3.1
+// @version      6.3.2
+// @version      6.3.2
 // @description  Mark up YouTube videos and quickly generate clipped webms.
 // @author       elwm
 // @namespace    https://github.com/codedealer
@@ -35,7 +35,7 @@
 // ==/UserScript==
 // BANNER GUARD
 
-const __version__ = '6.3.1';
+const __version__ = '6.3.2';
 import { Chart, ChartConfiguration } from 'chart.js';
 import { safeSetInnerHtml } from './util/util';
 import { stripIndent } from 'common-tags';
